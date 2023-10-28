d51a22813bf29c54c105a87f6b8090aa20003536
Yohaniii25 <yohanii725@gmail.com>
2023-10-28 14:00:45 +0530

    complete faq

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,31 @@
 var faq = document.getElementsByClassName("faq-page");
 var i;
+var currentQuestion = faq[0]; // Make the first question active by default
 
 for (i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
+        // Close the currently active question (if any)
+        if (currentQuestion !== this) {
+            currentQuestion.classList.remove("active");
+            var currentAnswer = currentQuestion.nextElementSibling;
+            currentAnswer.style.display = "none";
+        }
 
-        /* Toggle between hiding and showing the active panel */
+        // Toggle the clicked question
+        this.classList.toggle("active");
         var body = this.nextElementSibling;
         if (body.style.display === "block") {
             body.style.display = "none";
         } else {
             body.style.display = "block";
         }
+
+        // Update the currently active question
+        currentQuestion = this;
     });
 }
+
+// Initialize the first question as active
+currentQuestion.classList.add("active");
+var firstAnswer = currentQuestion.nextElementSibling;
+firstAnswer.style.display = "block";
